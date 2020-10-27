5dddca05f291e6bcb19404d0979253c0ea4ad62f
Paulo Alberto Sousa <p.sousa612@gmail.com>
2020-10-27 15:35:15 -0700

    a lot

--- react-app/src/components/Leaderboard.js
+++ react-app/src/components/Leaderboard.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './style.scss';
+import './leaderboard.scss';
+
+import RowItem from './RowItem';
 
 const Leaderboard = () => {
   return (
-    <div>
+    <div class="panel">
       <h1> Leaderboard </h1>
+      
+      <table>
+        <thead>
+        <tr>
+          <th>Ranking</th>
+          <th>Username</th>
+          <th>Points</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {/*Put the api call and create row items for each leaderboard row*/}
+      <RowItem ranking="1" username="yash" points="300"/>
+      <RowItem ranking="2" username="bob" points="299"/>
+      </tbody>
+
+      </table>
+      
     </div>
   );
 }
--- react-app/src/components/Navigation.js
+++ react-app/src/components/Navigation.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './navigation.scss';
 
 const Navigation = () => {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/leaderboard">Leaderboard</NavLink>
+    <div class="navbar">
+      <ul>
+      <li><NavLink to="/" class="navlink">Home</NavLink></li>
+      <li><NavLink to="/leaderboard" class="navlink">Leaderboard</NavLink></li>
+      <li><NavLink to="/leaderboard" class="profile-pic">Leaderboard</NavLink></li>
+
+      </ul>
     </div>
   );
 }
