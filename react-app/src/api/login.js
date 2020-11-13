85fa292b317cbcfedb71da83434b934f6add3c58
Paulo Alberto Sousa <p.sousa612@gmail.com>
2020-11-13 02:39:50 -0800

    yer

---
+++ react-app/src/api/login.js
@@ -0,0 +1,12 @@
+export const login = (username, password) => {
+    
+    const requestOpts = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({"username":username, "password":password})
+    }
+
+    fetch('/login', requestOpts)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
