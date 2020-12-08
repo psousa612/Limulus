46ba923452b5194c3930879aaed21c2126bfcd40
Paulo Alberto Sousa <p.sousa612@gmail.com>
2020-12-08 15:48:14 -0800

    ok

--- src/api/leaderboard.js
+++ src/api/leaderboard.js
@@ -1,13 +1,18 @@
-export const leaders = async() => {
+export const leaders = async (username) => {
     var l;
 
-    await fetch('https://limulus0.herokuapp.com/leaderboard')
+    const requestOpts = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({"username":username})
+    }
+    await fetch('https://limulus0.herokuapp.com/leaderboard', requestOpts)
         .then(response => response.json())
         .then(data => l = data)
 
     // console.log("From API: ")
     // console.log(l);
-    var board = l["leaderboard"]
+    // var board = l["leaderboard"]
 
-    return board;
+    return l;
 }
